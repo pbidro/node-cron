5c3109b798d424a16d3a08938b3d34bd887e0083
Pablo Iván Bustamante Idro <pb.idro94@gmail.com>
2022-11-16 23:18:40 -0300

    pablito test

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ import cron from 'node-cron';
 
 cron.schedule(`*/1 * * * *`, async () => {
   console.log(`running your task...`);
+  console.log(new Date().toLocaleString());
+  console.log(`pablito`);
+
 });
